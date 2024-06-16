763f6d78f4de2b7098a9043175acf410484d26d3
Mohamed <mohamed.laaribi.dev@gmail.com>
2024-06-16 16:35:14 +0200

    Update experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -110,15 +110,15 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    company_name: "Dynaskills",
+
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2024 - present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+
     ],
   },
   {
